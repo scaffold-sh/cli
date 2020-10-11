01e3e4bd76749dd13d4c1a65d8779fa830bc0cb6
Jeremy Levy <jje.levy@gmail.com>
2020-10-11 18:15:54 +0200

    Fix build output dir env var set to 'undefined' when no build command

--- src/lib/infrastructures/aws/AWSStaticWebsite.ts
+++ src/lib/infrastructures/aws/AWSStaticWebsite.ts
@@ -131,7 +131,7 @@ class AWSStaticWebsite implements IInfrastructure<IAWSStaticWebsiteEnvVars> {
       } = await buildCommandPrompt(defaults.BUILD_COMMAND, defaults.BUILD_OUTPUT_DIR)
 
       envVars.BUILD_COMMAND = buildCommand
-      envVars.BUILD_OUTPUT_DIR = buildOutputDir
+      envVars.BUILD_OUTPUT_DIR = buildOutputDir || ""
     }
 
     if (!hasGlobalEnv || typeof defaults.ENABLE_HTTPS !== "undefined") {
